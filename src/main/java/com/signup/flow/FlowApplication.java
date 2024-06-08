9428f2409ed7bbb7c9cec47108b276b6b727a26b
heaming <meetoohi@gmail.com>
2024-06-08 17:16:25 +0900

    [20240608] 접속 허용 스케줄러 * @EnableScheduling

--- src/main/java/com/signup/flow/FlowApplication.java
+++ src/main/java/com/signup/flow/FlowApplication.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationListener;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FlowApplication  {
     public static void main(String[] args) {
--- src/main/java/com/signup/flow/service/UserQueueService.java
+++ src/main/java/com/signup/flow/service/UserQueueService.java
@@ -2,22 +2,32 @@ package com.signup.flow.service;
 
 import com.signup.flow.FlowApplication;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.data.redis.connection.zset.Tuple;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
 import static com.signup.flow.exception.ErrorCode.QUEUE_ALREADY_REGISTERED_USER;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+    @Value("${scheduler.enabled}")
+    private boolean scheduling = false;
 
     // 대기열 등록 API
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -38,7 +48,7 @@ public class UserQueueService {
     // - 진입이 가능한 상태인지 조회
     // - 진입 허용
     /**
-     * @Title 진입 요청
+     * @apiNote  진입 요청
      * @param queue
      * @param count 몇 개의 유저를 허용할 것인지 정의
      * @return Long 몇 명이 허용되었는지
@@ -54,7 +64,7 @@ public class UserQueueService {
     }
 
     /**
-     * @Title 진입 가능 여부 확인
+     * @apiNote 진입 가능 여부 확인
      * @param queue
      * @param userId
      * @return Boolean
@@ -67,7 +77,7 @@ public class UserQueueService {
     }
 
     /**
-     * @Title 대기열 순번 확인
+     * @apiNote 대기열 순번 확인
      * @param queue
      * @param userId
      * @return Long
@@ -80,6 +90,28 @@ public class UserQueueService {
     }
 
 
+    /**
+     * @apiNote 대기자 허용 스케줄러
+     */
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+        if(!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
 
+        log.info("called scheduling...");
 
+        var maxAllowUserCount = 3L;
+
+        // 사용자 허용
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+                .subscribe();
+    }
 }
