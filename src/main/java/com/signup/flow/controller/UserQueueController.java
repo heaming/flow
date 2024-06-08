fc7412b95fcd60964dc709a00c1fc263159efc64
heaming <meetoohi@gmail.com>
2024-06-08 15:56:45 +0900

    [20240607] 접속대기 웹페이지, 대기번호 확인 API, allow시 redirect

--- src/main/java/com/signup/flow/controller/UserQueueController.java
+++ src/main/java/com/signup/flow/controller/UserQueueController.java
@@ -2,6 +2,7 @@ package com.signup.flow.controller;
 
 import com.signup.flow.dto.AllowUserResponse;
 import com.signup.flow.dto.AllowedUserResponse;
+import com.signup.flow.dto.RankNumberResponse;
 import com.signup.flow.dto.RegisterUserResponse;
 import com.signup.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
@@ -36,6 +37,14 @@ public class UserQueueController {
 
         return userQueueService.isAllowed(queue, userId)
                 .map(AllowedUserResponse::new);
+    }
+
+    @GetMapping("/rank")
+    public Mono<RankNumberResponse> getRankUser(@RequestParam(name="queue", defaultValue = "default") String queue,
+                                                @RequestParam(name="user_id") Long userId) {
+
+        return userQueueService.getRank(queue, userId)
+                .map(RankNumberResponse::new);
     }
 
 }
---
+++ src/main/java/com/signup/flow/dto/RankNumberResponse.java
@@ -0,0 +1,4 @@
+package com.signup.flow.dto;
+
+public record RankNumberResponse(Long rank) {
+}
